feae114cbee347fa98860e95fb21fac9365ff146
upl8-dev <eshirley1@byupathway.edu>
2026-05-16 16:17:25 -0700

    1. Added _blank to "Old" 2. Adjusted JavaScript to include menu and navigation.

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,6 +1,9 @@
 const today = document.getElementById("lastModified").innerHTML = document.lastModified;
 const year = document.getElementById("currentyear").textContent = new Date().getFullYear();
 
+const menuButton = document.querySelector("#menu");
+const navigation = document.querySelector("#navigation");
+
 menuButton.addEventListener("click", () => {
     navigation.classList.toggle("open");
     menuButton.classList.toggle("open");
